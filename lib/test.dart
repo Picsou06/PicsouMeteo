6f993e0fc0bdb6dda0e7f5f6c863dc2b7d85a85f
Picsou06 <110305876+Picsou06@users.noreply.github.com>
2023-01-17 08:54:44 +0100

    mise a jiour test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -16,14 +16,8 @@ class meteoontime {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['cod'] = this.cod;
-    data['message'] = this.message;
-    data['cnt'] = this.cnt;
     if (this.list != null) {
       data['list'] = this.list!.map((v) => v.toJson()).toList();
-    }
-    if (this.city != null) {
-      data['city'] = this.city!.toJson();
     }
     return data;
   }
